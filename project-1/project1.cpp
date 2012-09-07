7b595264e22a484e610c3fa5c163458acacc04d3
lancepetersen <petersenlance@yahoo.com>
2012-09-06 17:59:27 -0700

    Makefile project1

---
+++ project-1/project1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "CS3060" << endl;
+	cout << "Lance Petersen" << endl;
+	cout << argv[1] << endl << argv[2] << endl;
+	return 0;
+}
+
